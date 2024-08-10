3b7d39582fe57ccd5ddd0b6a7adde8014553eb1d
Mwenya Sikazwe <mwenyask17@icloud.com>
2024-08-10 04:45:31 +0300

    Fixed DB

--- frontend/src/components/main/MainContent.js
+++ frontend/src/components/main/MainContent.js
@@ -3,61 +3,9 @@ import './MainContent.css';
 import axios from 'axios';
 
 function MainContent() {
-  const [players, setPlayers] = useState([]);
-
-  useEffect(() => {
-    axios.get('/api/players')
-      .then(response => {
-        setPlayers(response.data);
-      })
-      .catch(error => {
-        console.error("There was an error fetching the player data!", error);
-      });
-  }, []);
 
   return (
     <main className="main-content">
-      <section className="renamethis">
-        <h2>Player Stats</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Nation</th>
-              <th>Position</th>
-              <th>Age</th>
-              <th>Matches Played</th>
-              <th>Starts</th>
-              <th>Minutes Played</th>
-              <th>Goals</th>
-              <th>Assists</th>
-              <th>xG</th>
-              <th>xAG</th>
-              <th>npxG</th>
-              <th>npxGxAG</th>
-            </tr>
-          </thead>
-          <tbody>
-            {players.map(player => (
-              <tr key={player.id}>
-                <td>{player.player_name}</td>
-                <td>{player.nation}</td>
-                <td>{player.position}</td>
-                <td>{player.age}</td>
-                <td>{player.matches_played}</td>
-                <td>{player.starts}</td>
-                <td>{player.minutes_played}</td>
-                <td>{player.goals}</td>
-                <td>{player.assists}</td>
-                <td>{player.xG}</td>
-                <td>{player.xAG}</td>
-                <td>{player.npxG}</td>
-                <td>{player.npxGxAG}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </section>
       <hr className="divider"/>
       <section className="placeholder">
         <h2>Placeholder</h2>
